53375d8543469023799878873736e048ebf13aac
sjs <shan@gmail.com>
2025-08-29 15:51:23 +0530

    today update

---
+++ Test.java
@@ -0,0 +1,14 @@
+//Explicit Type casting
+class Test
+{
+	public static void main(String[] args)
+	{
+		double d=130.345;
+		
+		byte b=(byte)d;
+		//System.out.println(x);
+		System.out.println(b);
+		
+
+	}
+}
